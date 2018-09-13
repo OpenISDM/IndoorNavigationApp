38481702721fe59f963b70ed9d0d2be09c297ae6
唐振嚴 <kenneth@nssh.ntpc.edu.tw>
2018-09-13 17:55:30 +0800

    design signal process

---
+++ IndoorNavigation/IndoorNavigation/Models/NavigationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndoorNavigation.Models
+{
+    public class BeaconSignalModel
+    {
+        public Guid UUID { get; set; }
+        /// <summary>
+        /// IBeacon Major field
+        /// </summary>
+        public int Major { get; set; }
+        /// <summary>
+        /// IBeacon Minor field
+        /// </summary>
+        public int Minor { get; set; }
+        public int TxPower { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ IndoorNavigation/IndoorNavigation/Modules/SignalProcessing.cs
@@ -0,0 +1,60 @@
+﻿using IndoorNavigation.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Linq;
+
+namespace IndoorNavigation.Modules
+{
+    public class SignalProcess:IDisposable
+    {
+        private Thread SignalProcessThread;
+        private  List<BeaconSignalModel> BeaconSignalBuffer = 
+            new List<BeaconSignalModel>();
+        private bool Switch = true;
+        private object BufferLock = new object();
+
+        public SignalProcess()
+        {
+            SignalProcessThread = 
+                new Thread(SignalProcessWork){ IsBackground = true};
+            SignalProcessThread.Start();
+        }
+
+        public void AddSignal(List<BeaconSignalModel> Signals)
+        {
+            lock (BufferLock)
+                BeaconSignalBuffer.AddRange(Signals);
+        }
+
+        public void SignalProcessWork()
+        {
+            while (Switch)
+            {
+                List<BeaconSignalModel> SignalAverageList = new List<BeaconSignalModel>();
+                // SignalProcess
+                // remove buffer old data
+                lock (BufferLock)
+                {
+                    foreach (var BeaconSignal in BeaconSignalBuffer.Where(c => c.Timestamp < DateTime.Now.AddMilliseconds(-500)))
+                        BeaconSignalBuffer.Remove(BeaconSignal);
+
+                    //foreach (var BeaconUUID in BeaconSignalBuffer.Select(c => c.UUID).Distinct())
+                }
+                    
+
+
+                // wait 500s or wait module close
+                SpinWait.SpinUntil(() => Switch, 500);
+            }
+
+            Debug.WriteLine("Signal process close");
+        }
+
+        public void Dispose()
+        {
+            Switch = false;
+        }
+    }
+}
