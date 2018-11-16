ff742dfccffa1af084482dcad5fdf1e246288118
ohnoholly <hollywang0728@hotmail.com>
2018-11-16 12:31:01 +0800

    Comments

--- IndoorNavigation/IndoorNavigation/Modules/Navigation/RotePlan.cs
+++ IndoorNavigation/IndoorNavigation/Modules/Navigation/RotePlan.cs
@@ -2,27 +2,27 @@
  * Copyright (c) 2018 Academia Sinica, Institude of Information Science
  *
  * License:
- *      GPL 3.0 : The content of this file is subject to the terms and 
+ *      GPL 3.0 : The content of this file is subject to the terms and
  *      conditions defined in file 'COPYING.txt', which is part of this source
  *      code package.
  *
  * Project Name:
- * 
+ *
  *      IndoorNavigation
- * 
+ *
  * File Description:
  * File Name:
- * 
+ *
  *      RotePlan.cs
- * 
+ *
  * Abstract:
- *      
- *      規劃導航路徑的演算法
+ *
+ *      The algorithm of tour planning
  *
  * Authors:
- * 
+ *
  *      Kenneth Tang, kenneth@gm.nssh.ntpc.edu.tw
- * 
+ *
  */
 
 using System.Collections.Generic;
@@ -35,8 +35,8 @@ using System;
 namespace IndoorNavigation.Modules.Navigation
 {
     /// <summary>
-    /// 路徑規劃
-    /// 使用Dijkstra演算法
+    /// rout plan
+    /// Dijkstra algorithm
     /// </summary>
     public class RoutePlan
     {
@@ -45,28 +45,27 @@ namespace IndoorNavigation.Modules.Navigation
         private readonly List<LocationConnectModel> locationConnects;
 
         /// <summary>
-        /// 初始化路徑規劃物件
+        /// Initialize the element
         /// </summary>
-        /// <param name="BeaconGroups">地點資料</param>
-        /// <param name="LocationConnects">連接兩個地點的道路資料</param>
+        /// <param name="BeaconGroups">Location information </param>
+        /// <param name="LocationConnects">the path information </param>
         public RoutePlan(List<BeaconGroupModel> BeaconGroups,
             List<LocationConnectModel> LocationConnects)
         {
-            // 加入地點
+            // Add a location
             foreach (var BeaconGroup in BeaconGroups)
                 map.AddNode(BeaconGroup);
 
-            // 設定道路
+            // Set the path
             this.locationConnects = LocationConnects;
             foreach (var locationConnect in LocationConnects)
             {
-                // 取得兩個地點間的距離
-                // 距離以公分為單位
+                // Get the distance of two locations which in cm.
                 int distance = System.Convert.ToInt32(
                     locationConnect.BeaconA.Coordinate
                     .GetDistanceTo(locationConnect.BeaconB.Coordinate) * 100);
 
-                // 找出要連接兩個地點的key值
+                // Get two connected location's key vaule
                 uint beaconAKey = map.Where(BeaconGroup =>
                         BeaconGroup.Item == locationConnect.BeaconA)
                         .Select(BeaconGroup => BeaconGroup.Key).First();
@@ -74,7 +73,7 @@ namespace IndoorNavigation.Modules.Navigation
                         BeaconGroup.Item == locationConnect.BeaconB)
                         .Select(BeaconGroup => BeaconGroup.Key).First();
 
-                // 將兩個地點連接起來
+                // Connect two locations
                 if (locationConnect.IsTwoWay)
                 {
                     map.Connect(beaconAKey,beaconBKey,distance,string.Empty);
@@ -86,7 +85,7 @@ namespace IndoorNavigation.Modules.Navigation
         }
 
         /// <summary>
-        /// 取得最佳路線
+        /// Get the best path
         /// </summary>
         /// <param name="StartBeacon"></param>
         /// <param name="EndPoint"></param>
@@ -94,39 +93,42 @@ namespace IndoorNavigation.Modules.Navigation
         public Queue<NextInstructionModel> GetPath(
             Beacon StartBeacon, BeaconGroupModel EndPoint)
         {
-            // 先找出start beacon在哪個地點
-            // 再找出這個地點的key值
+            // Find where is the the start beacon and find the key vaule
             uint startPoingKey = map
                 .Where(BeaconGroup => BeaconGroup.Item.Beacons
                 .Contains(StartBeacon)).Select(c => c.Key).First();
             uint endPointKey = map
                 .Where(c => c.Item == EndPoint).Select(c => c.Key).First();
-            // 取得最佳路線
+            // Get the best path
             var path = map.Dijkstra(startPoingKey, endPointKey).GetPath();
 
             Queue<NextInstructionModel> pathQueue =
                 new Queue<NextInstructionModel>();
-            // 計算走向下一個地點要旋轉的角度
+            // Compute the angle to the next location
             for (int i = 0; i < path.Count() - 1; i++)
             {
-                // 取出現在的地點和下一個地點
+                // Get the current location and next location
                 BeaconGroupModel currentPoint = map[path.ToList()[i]].Item;
                 BeaconGroupModel nextPoint = map[path.ToList()[i + 1]].Item;
 
-                // i=0代表要計算起點的方向
+                // If i=0, it represented that it needs to compute the initial
+                // directino fo start point
                 if (i == 0)
                 {
-                    // 因為要先讓使用者走錯路，再校正方向。所以，不計算方向
+                    // Don't need to compute the direction because user should
+                    // get the wrong way first, and then calibrate the direction
                     pathQueue.Enqueue(new NextInstructionModel
                     {
                         NextPoint = nextPoint,
                         Angle = int.MaxValue
                     });
 
-                    // 保留功能
-                    // 之前開會討論要在LBeacon上貼標籤來校正起始方向
-                    //// 檢查起始點是否為LBeacon
-                    //// LBeacon上方有提示使用者一開始要面向的方向
+                    // Reserved function
+                    // According to the meeting before, there is a sticker on
+                    // LBeacon to show the initial direction
+                    //// Check the starting point if is under the LBeacon
+                    //// There is a sign on LBeacon to show the user where is
+                    //// the diresction to face first. 
                     //if (StartBeacon.GetType() == typeof(LBeaconModel))
                     //    pathQueue.Enqueue(new NextInstructionModel
                     //    {
@@ -146,11 +148,12 @@ namespace IndoorNavigation.Modules.Navigation
                 }
                 else
                 {
-                    // 上一個地點
-                    BeaconGroupModel previousPoint = 
+                    // Last location
+                    BeaconGroupModel previousPoint =
                         map[path.ToList()[i - 1]].Item;
 
-                    // 計算要轉向的角度，連同下一個位置一併放到路線佇列
+                    // Compute the angle to turn. It is also be stored with
+                    // next location to the path queue.
                     int angle = RotateAngle.GetRotateAngle(
                         currentPoint.Coordinate,
                         previousPoint.Coordinate,
@@ -167,7 +170,7 @@ namespace IndoorNavigation.Modules.Navigation
         }
 
         /// <summary>
-        /// 重新取得最佳路線
+        /// Reacquire the best path
         /// </summary>
         /// <param name="PreviousPoint"></param>
         /// <param name="CurrentBeacon"></param>
@@ -178,16 +181,16 @@ namespace IndoorNavigation.Modules.Navigation
             Beacon CurrentBeacon,
             BeaconGroupModel EndPoint)
         {
-            // 先找出start beacon在哪個地點
-            // 再找出這個地點的key值
+            // Find where is the the start beacon and find the key vaule
             var startPoingKey = map
                 .Where(beaconGroup => beaconGroup.Item.Beacons
                 .Contains(CurrentBeacon)).Select(c => c.Key).First();
             var endPointKey = map
                 .Where(c => c.Item == EndPoint).Select(c => c.Key).First();
 
-            // 檢查上一個位置與現在位置是否連接
-            // 假如使用者跳過2個地點以上，上一個位置就沒有參考價值
+            // Check the current location if is connected to the last location.
+            // If the user skips more than two location, the last location is
+            // less meaningful.
             if (this.locationConnects.Where(c =>
             (c.BeaconA == map[startPoingKey] && c.BeaconB == PreviousPoint) ||
             (c.BeaconA == PreviousPoint && c.BeaconB == map[startPoingKey]))
@@ -196,23 +199,24 @@ namespace IndoorNavigation.Modules.Navigation
                     "The current point is independent of the previous point."
                     );
 
-            // 取得最佳路線
+            // Get the best path
             var path = map.Dijkstra(startPoingKey, endPointKey).GetPath();
 
             Queue<NextInstructionModel> pathQueue =
                 new Queue<NextInstructionModel>();
 
-            // 計算走向下一個地點要旋轉的角度
+            // Compute the angle to turn to next location
             for (int i = 0; i < path.Count() - 1; i++)
             {
-                // 取出現在的地點和下一個地點
+                // Acquire the current and next location
                 BeaconGroupModel currentPoint = map[path.ToList()[i]].Item;
                 BeaconGroupModel nextPoint = map[path.ToList()[i + 1]].Item;
 
 
                 if (i == 0)
                 {
-                    // 重新導航的第一個點可以使用上一個位置計算轉向角度
+                    // The function of redirection can use last node to compute
+                    // the angle to turn
                     int angle = RotateAngle.GetRotateAngle(
                         currentPoint.Coordinate,
                         PreviousPoint.Coordinate,
@@ -226,7 +230,7 @@ namespace IndoorNavigation.Modules.Navigation
                 }
                 else
                 {
-                    // 使用路線佇列中的上一個點計算轉向角度
+                    // Use the last node in the queue to compute the angle
                     PreviousPoint = map[path.ToList()[i - 1]].Item;
                     int angle = RotateAngle.GetRotateAngle(
                         currentPoint.Coordinate,
