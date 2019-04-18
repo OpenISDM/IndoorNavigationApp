14ae4360359a758ba31b6c75c7cd4ed7f9459f89
Hsin Han Wu <evan.sh.wu@gmail.com>
2019-04-18 10:02:42 +0800

    Update NavigationLayer.cs
    Update abstract content

--- IndoorNavigation/IndoorNavigation/Models/NavigationLayer.cs
+++ IndoorNavigation/IndoorNavigation/Models/NavigationLayer.cs
@@ -22,10 +22,15 @@
  *
  * Abstract:
  *
- *      Waypoint-based navigate is a mobile indoor navigation application that 
- *      runs on smart phones. It relies on BeDIS (Building/environment Data and Information System) 
- *      for indoor positioning. This version makes use of Xamarin.Forms, 
- *      which is a complete cross-platform UI tookit that runs on both iOS and Android.
+ *      Waypoint-based navigator is a mobile Bluetooth navigation application that 
+ *      runs on smart phones. It is structed to support anywhere navigation. Indoors 
+ *      in areas covered by different indoor positioning system (IPS) and outdoors
+ *      covered by GPS. In particilar, it can rely on BeDIS 
+ *      (Building/environment Data and Information System) for indoor positioning. 
+ *      Using this IPS, the navigator does not need to continuously monitor its own 
+ *      position, since the IPS broadcast to the navigator the location of each waypoint.
+ *      This version makes use of Xamarin.Forms, which is a complete cross-platform 
+ *      UI tookit that runs on both iOS and Android.
  *
  * Authors:
  *
