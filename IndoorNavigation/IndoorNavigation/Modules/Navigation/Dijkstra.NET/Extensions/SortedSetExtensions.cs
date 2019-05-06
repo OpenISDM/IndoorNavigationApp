42fdd6e0978271e45d692bea03e737849912db26
Paul <a2323269@gmail.com>
2019-05-06 12:03:26 +0800

    Add the Dijkstra.NET library from github.com/matiii/Dijkstra.NET
    This library is reference https://github.com/matiii/Dijkstra.NET and its version is 1.1.0.

---
+++ IndoorNavigation/IndoorNavigation/Modules/Navigation/Dijkstra.NET/Extensions/SortedSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dijkstra.NET.Extensions
+{
+    internal static class SortedSetExtensions
+    {
+        public static T Deque<T>(this SortedSet<T> set)
+        {
+            T item = set.First();
+
+            set.Remove(item);
+
+            return item;
+        }
+    }
+}
---
+++ IndoorNavigation/IndoorNavigation/Modules/Navigation/Dijkstra.NET/Model/INode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dijkstra.NET.Delegates;
+
+namespace Dijkstra.NET.Contract
+{
+    public interface INode<T,TEdgeCustom> where TEdgeCustom : IEquatable<TEdgeCustom>
+    {
+        T Item { get; }
+
+        uint Key { get; }
+
+        void EachChild(ChildAction<T, TEdgeCustom> action);
+    }
+}
