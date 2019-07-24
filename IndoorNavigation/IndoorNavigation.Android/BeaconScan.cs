ab5dfa96d553dbc8a44dcd21166a674b74dae596
wang-chi <h.wangchi.0970@gmail.com>
2019-07-24 11:21:45 +0800

    Modify Android BeaconScan

--- IndoorNavigation/IndoorNavigation.Android/BeaconScan.cs
+++ IndoorNavigation/IndoorNavigation.Android/BeaconScan.cs
@@ -42,13 +42,9 @@
  *      Wang-Chi Ho, h.wangchi.0970@gmail.com
  *
  */
+using Android.Bluetooth;
 using IndoorNavigation.Droid;
 using IndoorNavigation.Models;
-using Plugin.BLE;
-using Plugin.BLE.Abstractions;
-using Plugin.BLE.Abstractions.Contracts;
-using Plugin.BLE.Abstractions.EventArgs;
-using Plugin.BLE.Abstractions.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,125 +52,91 @@ using System.Linq;
 [assembly: Xamarin.Forms.Dependency(typeof(BeaconScan))]
 namespace IndoorNavigation.Droid
 {
-    public class BeaconScan : IBeaconScan
+    public class BeaconScan : Java.Lang.Object, BluetoothAdapter.ILeScanCallback, LBeaconScan
     {
-        IBluetoothLE _bluetooth = CrossBluetoothLE.Current;
-        IAdapter _bleAdapter = CrossBluetoothLE.Current.Adapter;
-
-        private int _rssiThreshold = -50;
+        protected BluetoothAdapter _adapter;
+        protected BluetoothManager _manager;
+        private int _count = 0;
+        private int _rssiThreshold = -45;
 
         public NavigationEvent _event { get; private set; }
 
         public BeaconScan()
         {
             _event = new NavigationEvent();
-            this._bleAdapter.DeviceDiscovered += this.DeviceDiscovered;
+            var appContext = Android.App.Application.Context;
+            this._manager = (BluetoothManager)appContext.GetSystemService("bluetooth");
+            this._adapter = this._manager.Adapter;
+        }
 
-            Console.WriteLine("In BeaconScan constructor: IAdapter state = " +
-                              this._bluetooth.State);
-            this._bleAdapter.ScanTimeout = 1500;
+        public void StartScan()
+        {
+            this._count = 0;
+            this._adapter.StartLeScan(this);
+        }
+
+        public void StopScan()
+        {
+            this._adapter.StopLeScan(this);
+        }
+
+        public void OnLeScan(BluetoothDevice bleDevice, int rssi, byte[] scanRecord)
+        {
+            this._count = this._count + 1;
+            if (rssi > _rssiThreshold && rssi < 0)
+            {
+                string tempUUID = BitConverter.ToString(scanRecord);
+                string identifierUUID = ExtractBeaconUUID(tempUUID);
+                Console.WriteLine("\n >> Find A Beacon[{0}] Id:{1}; RSSI:{2}; Record:{3}\n", this._count, bleDevice.Address, rssi, identifierUUID);
+                if (identifierUUID.Length >= 36)
+                {
+                    List<BeaconSignalModel> signals = new List<BeaconSignalModel>();
+                    signals.Add(new BeaconSignalModel
+                    {
+                        UUID = new Guid(identifierUUID),
+                        RSSI = rssi
+                    });
+
+                    _event.OnEventCall(new BeaconScanEventArgs
+                    {
+                        _signals = signals
+                    });
+                }
+            }
         }
 
         public void Close()
         {
         }
 
-        private void DeviceDiscovered(object sender, DeviceEventArgs args)
-        {
-            Console.WriteLine("Find A Beacon Id:{0}; RSSI:{1}", (args as DeviceEventArgs).Device.Id, (args as DeviceEventArgs).Device.Rssi);
-            if ((args as DeviceEventArgs).Device.Rssi > _rssiThreshold &&
-                (args as DeviceEventArgs).Device.Rssi < 0)
-            {
-                var tempUUID = (string)null;
-                foreach (AdvertisementRecord record in (args as DeviceEventArgs).Device.AdvertisementRecords)
-                {
-                    if (record.Type == AdvertisementRecordType.ManufacturerSpecificData)
-                    {
-                        /* Sample of ManufacturerSpecificData data:
-                         * Ade = {
-                         *    [
-                         *      Type ManufacturerSpecificData; 
-                         *      Data 0F-00-02-15-00-00-00-18-00-00-00-00-66-60-00-00-00-01-19-00-00-02-00-00-CE
-                         *    ]
-                         *  }
-                         */
-                        tempUUID = string.Format("{0}", record.Data.ToHexString());
-                        break;
-                    }
-                }
-
-                if (tempUUID != null)
-                {
-                    string bufferUUID = tempUUID.ToString();
-                    string identifierUUID = ExtractBeaconUUID(bufferUUID);
-                    Console.WriteLine("Find Beacon Id:{0}; RSSI:{1}", identifierUUID, (args as DeviceEventArgs).Device.Rssi);
-
-                    if (identifierUUID.Length >= 36)
-                    {
-                        List<BeaconSignalModel> signals = new List<BeaconSignalModel>();
-                        signals.Add(new BeaconSignalModel
-                        {
-                            UUID = new Guid(identifierUUID),
-                            RSSI = (args as DeviceEventArgs).Device.Rssi
-                        });
-
-                        _event.OnEventCall(new BeaconScanEventArgs
-                        {
-                            _signals = signals
-                        });
-                    }
-
-                }
-            }
-
-        }
-
         public void Dispose()
         {
             Console.WriteLine(">> Beacon Scan Dispose");
-            this._bleAdapter.DeviceDiscovered -= this.DeviceDiscovered;
         }
+
+        private void UpdatedState(object sender, EventArgs args)
+        {
+        }
+
 
         private string ExtractBeaconUUID(string stringAdvertisementSpecificData)
         {
             string[] parse = stringAdvertisementSpecificData.Split("-");
 
-            if (parse.Count() < 20)
+            if (parse.Count() < 60)
             {
                 return stringAdvertisementSpecificData;
             }
             else
             {
                 var parser = string.Format("{0}{1}{2}{3}-{4}{5}-{6}{7}-{8}{9}-{10}{11}{12}{13}{14}{15}",
-                                            parse[4], parse[5], parse[6], parse[7],
-                                            parse[8], parse[9],
-                                            parse[10], parse[11],
-                                            parse[12], parse[13],
-                                            parse[14], parse[15], parse[16], parse[17], parse[18], parse[19]);
+                                            parse[9], parse[10], parse[11], parse[12],
+                                            parse[13], parse[14],
+                                            parse[15], parse[16],
+                                            parse[17], parse[18],
+                                            parse[19], parse[20], parse[21], parse[22], parse[23], parse[24]);
                 return parser.ToString();
             }
         }
-
-        public void StartScan()
-        {
-            if (BluetoothState.On == this._bluetooth.State)
-            {
-                Console.WriteLine(">> Beacon Scan Start");
-                Guid[] uuid = { };
-                this._bleAdapter.StartScanningForDevicesAsync(uuid, null, true, default);
-            }
-        }
-
-        public void StopScan()
-        {
-            Console.WriteLine(">> Beacon Scan Stop");
-            this._bleAdapter.StopScanningForDevicesAsync();
-        }
-
-        private void UpdatedState(object sender, EventArgs args)
-        {
-
-        }
-
     }
 }
